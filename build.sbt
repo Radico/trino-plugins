5836f394bee5df659df46f508745e213fdc405ab
Joel Edwards <jedwards@simondata.com>
2021-07-22 23:48:08 -0600

    feat: update to trino 352

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 //version := "1.0"
 
 val projectName = "trino-plugins"
-val trinoVersion = "351"
+val trinoVersion = "352"
 
 // Should the com.simondata.trino.Run object be exported in the jar?
 val addEntryPoint = true
--- src/test/scala/com/simondata/trino/CustomSystemAccessControlSpec.scala
+++ src/test/scala/com/simondata/trino/CustomSystemAccessControlSpec.scala
@@ -7,10 +7,7 @@ class CustomSystemAccessControlSpec extends UnitSpec {
   "CustomSystemAccessControl" when {
     "defining custom methods" should {
       "override all methods declared on SystemAccessControl" in {
-        println(s"Inspecting io.trino.spi.security.SystemAccessControl ...")
         val sacInfo = XRay.reflectClassName("io.trino.spi.security.SystemAccessControl")
-
-        println(s"Inspecting com.simondata.trino.CustomSystemAccessControl ...")
         val customInfo = XRay.reflectClassName("com.simondata.trino.CustomSystemAccessControl")
 
         val customMap = customInfo.methods.map({ method =>
@@ -21,18 +18,8 @@ class CustomSystemAccessControlSpec extends UnitSpec {
           customMap.get(sacMethod.name) match {
             case None => assert(false, s"No method matching name '${sacMethod.name}''")
             case Some(customMethod) => {
-              println(s"${sacMethod.name}()")
               sacMethod.parameters.zipAll(customMethod.parameters, null, null) foreach { case (sacParam, customParam) =>
-                //assert(sacParam.name == customParam.name)
                 assert(sacParam.valueType.name == customParam.valueType.name)
-                val aliasStr = {
-                  if (sacParam.name != customParam.name)
-                    s" [ALIASED ${customParam.name}]"
-                  else
-                    ""
-                }
-
-                println(s"  ${sacParam.name}: ${sacParam.valueType.name}${aliasStr}")
               }
             }
           }
